67b629897927e22893cac70d1ca3ed2d10a6ddbc
AuroraDelgado03 <adelgadov2001@alumno.ipn.mx>
2025-11-12 21:41:03 -0600

    Delete reto_uno.py

--- reto_uno.py
+++
@@ -1,9 +0,0 @@
-# Reto 1
-# 13/10/2025
-
-nombre = str(input("Hola usuario, ingresa tu nombre por favor: ")) # No es necesario agregar el str al inicio
-edad = int(input("Ingresa tu edad por favor: "))
-frase_fav = str(input("Ingresa tu frase favorita por favor: "))
-dinero = float(input("Ingresa la cantidad de dinero que tengas disponible por favor: "))
-
-print(f"Hola {nombre}, tienes {edad} años y tu frase favorita es: {frase_fav}. Tienes ${dinero} en tu cuenta")
